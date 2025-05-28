22fbbf5b2291a01dc5fad8037746f57dfb2f8004
Alan orfano <112592488+AlanOrfano@users.noreply.github.com>
2025-05-28 20:11:07 -0300

    Update pagina de venta javascript.js

--- pagina de venta javascript.js
+++ pagina de venta javascript.js
@@ -26,23 +26,23 @@ function enviar()
                          } ;
        // envia al emailjs //
                         
-                    
+       const btn = document.getElementById('button');             
         
 
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   
+   btn.value = 'Sending...';
 
    const serviceID = 'default_service';
    const templateID = 'template_te47swg';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-     
+     btn.value = 'Send Email';
       alert('Listo!');
-      //this.reset();
+      this.reset();
       // la linea de abajo desactiva el boton al enviar los datos correctos, esto debido a unos errores al enviar los datos correctos
       // y apretar en el boton enviar con los datos vacios, envia un email vacio al servidor que no se tendria que enviar
       
