1f194f2be2ecf300b176a012e4c5ecdbf6739ba9
Alan orfano <112592488+AlanOrfano@users.noreply.github.com>
2025-06-02 18:47:43 -0300

    Update pagina de venta javascript.js

--- pagina de venta javascript.js
+++ pagina de venta javascript.js
@@ -1,15 +1,16 @@
 
-
-function enviar()
+function confirmar()
 {
+  
     var nombre= document.getElementById("nombre") .value;
     var email= document.getElementById("email") .value;
     var asunto= document.getElementById("asunto") .value;
     var mensaje= document.getElementById("mensaje") .value;
-    const recaptchaResponse = grecaptcha.getResponse();
-    
+    const btn = document.getElementById('boton'); 
+    const recaptchaResponse= grecaptcha.getResponse() .value;
+
     if (
-        !nombre || !email || !asunto || !mensaje || recarpchaResponse === ""
+        !nombre || !email || !asunto || !mensaje || !recarptchaResponse === ""
         )
         {
             alert ("complete los datos")
@@ -25,39 +26,40 @@ function enviar()
         mensaje: mensaje 
                          } ;
        // envia al emailjs //
-                        
-       const btn = document.getElementById('button');             
+                      
+                    
         
 
 document.getElementById('form')
  .addEventListener('submit', function(event) {
    event.preventDefault();
 
-   btn.value = 'Sending...';
+   
 
    const serviceID = 'default_service';
    const templateID = 'template_te47swg';
 
    emailjs.sendForm(serviceID, templateID, this)
     .then(() => {
-     btn.value = 'Send Email';
+      
       alert('Listo!');
       this.reset();
       // la linea de abajo desactiva el boton al enviar los datos correctos, esto debido a unos errores al enviar los datos correctos
       // y apretar en el boton enviar con los datos vacios, envia un email vacio al servidor que no se tendria que enviar
-      
+      btn.disabled = true;
       
     }, (err) => {
-      btn.value = 'Send Email';
+      
       alert(JSON.stringify(err));
+      alert ("ponga un email correcto")
     });
 });
 
 
        console.log(usuario) ;
 
-        alert ("analizaremos tu consulta, pronto recibiras respuestas")
+       
     
-    
-        }
- }
+
+}
+}
